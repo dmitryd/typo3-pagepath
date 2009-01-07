07a3ccae3cfcb21227503f86d14e85e7f0104637
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2009-01-07 08:55:36 +0000

    0.1.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "pagepath"
 #
-# Auto generated 27-10-2008 11:49
+# Auto generated 07-01-2009 10:41
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Page path',
-	'description' => 'Provides API to Backend modules to get a proper path to the Frontend page (simulateStatic/RealURL/CoolURI-like)',
+	'description' => 'Provides API for Backend modules to get a proper path to the Frontend page (simulateStatic/RealURL/CoolURI-like)',
 	'category' => 'be',
 	'author' => 'Dmitry Dulepov',
 	'author_email' => 'dmitry@typo3.org',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.0',
+	'version' => '0.1.3',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.2.0-4.99.99',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"a228";s:25:"class.tx_pagepath_api.php";s:4:"a6f9";s:30:"class.tx_pagepath_resolver.php";s:4:"fb1d";s:12:"ext_icon.gif";s:4:"a89b";s:17:"ext_localconf.php";s:4:"81af";s:14:"doc/manual.sxw";s:4:"7d24";s:19:"doc/wizard_form.dat";s:4:"488e";s:20:"doc/wizard_form.html";s:4:"5559";}',
+	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"7288";s:25:"class.tx_pagepath_api.php";s:4:"1d2c";s:30:"class.tx_pagepath_resolver.php";s:4:"631e";s:12:"ext_icon.gif";s:4:"a89b";s:17:"ext_localconf.php";s:4:"81af";s:14:"doc/manual.sxw";s:4:"7d24";}',
 	'suggests' => array(
 	),
 );
