7138de0f9c8d1c449bf2451e375ef51885d07623
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2009-12-14 10:13:50 +0000

    Released version 0.1.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "pagepath"
 #
-# Auto generated 29-01-2009 18:35
+# Auto generated 14-12-2009 12:13
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.4',
+	'version' => '0.1.5',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.2.0-4.99.99',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"4748";s:25:"class.tx_pagepath_api.php";s:4:"2e47";s:30:"class.tx_pagepath_resolver.php";s:4:"631e";s:12:"ext_icon.gif";s:4:"a89b";s:17:"ext_localconf.php";s:4:"81af";s:14:"doc/manual.sxw";s:4:"7d24";}',
+	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"490d";s:25:"class.tx_pagepath_api.php";s:4:"2e47";s:30:"class.tx_pagepath_resolver.php";s:4:"c4dc";s:12:"ext_icon.gif";s:4:"a89b";s:17:"ext_localconf.php";s:4:"81af";s:14:"doc/manual.sxw";s:4:"0f8f";}',
 	'suggests' => array(
 	),
 );
