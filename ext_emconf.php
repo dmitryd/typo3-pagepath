a77e7b6582f8d90133150800e30c3fb3b16e1f81
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2013-03-13 12:55:47 +0400

    Updated TYPO3 dependency version

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.0.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.5.0-0.0.0',
+			'typo3' => '4.5.0-6.1.999',
 			'php' => '5.3.2-10.0.0',
 		),
 		'conflicts' => array(
